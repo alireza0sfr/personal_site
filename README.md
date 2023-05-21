930c34de336af47b1b0f3efb5c7e1b6b2663ac9d
Alireza Safari <alireza.safaree@gmail.com>
2023-05-21 21:09:47 +0330

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hi There, I'm Alireza :)
+
+
+![Alireza Safari Github stats](https://github-readme-stats.vercel.app/api?username=alireza0sfr&show_icons=true)
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+document.getElementById('age').innerHTML = new Date().getFullYear() - 2002
